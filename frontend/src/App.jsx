7c3cf04cf4738240382ea80fb6967422f8b704d2
Johnny <jonathanbc2012@gmail.com>
2024-04-10 11:54:50 +0200

    frontend and backend created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      Hola, soy Parry y estoy muy loco que cuando voy a un hotel, hago la cama.
+    </div>
+  )
+}
+
+export default App
