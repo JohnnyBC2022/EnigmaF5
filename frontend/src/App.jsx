bd80893ba516db6dac710b15fcd1fabd9fd3917a
Johnny <jonathanbc2012@gmail.com>
2024-04-10 15:27:06 +0200

    audioplayer.jsx created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import AudioPlayer from './components/AudioPlayer'
 
 function App() {
   
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       Hola, soy Parry y estoy muy loco que cuando voy a un hotel, hago la cama.
+      <AudioPlayer/>
     </div>
   )
 }
---
+++ frontend/src/components/AudioPlayer.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+function AudioPlayer() {
+    useEffect(() => {
+        const audio = new Audio('https://api.soundcloud.com/tracks/1797647674/stream?secret_token=s-Akvs8H6OxEX');
+        audio.play();
+
+        return () => {
+            audio.pause();
+        };
+    }, []);
+
+    return (
+        <div >
+            <iframe
+                title="SoundCloud Player"
+                width="100%"
+                height="300"
+                scrolling="no"
+                frameborder="no"
+                allow="autoplay"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1797647674%3Fsecret_token%3Ds-Akvs8H6OxEX&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+            </iframe>
+        </div>
+    );
+}
+
+export default AudioPlayer;
