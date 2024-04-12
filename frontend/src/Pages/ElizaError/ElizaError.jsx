bfb887c52fdaac23ebcbd004819e46e3e0b3e8b7
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-12 10:59:45 +0200

    Added Dot Gothic font

--- frontend/src/Pages/ElizaError/ElizaError.jsx
+++ frontend/src/Pages/ElizaError/ElizaError.jsx
@@ -3,6 +3,13 @@ import "./ElizaError.css"
 export default function ElizaError() {
 
   return (
-    <div>ElizaError</div>
+    <main className="error-eliza">
+      <div className="corporation">MK Ultra Corp.</div>
+      <section className="central-card">
+        <p>ERROR AL ABRIR LA CONSOLA</p>
+        <p>Eliza System Failure</p>
+        <p>Cargando...</p>
+      </section>
+    </main>
   )
 }
