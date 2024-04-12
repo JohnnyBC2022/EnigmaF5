7be9fd6b9f23824bcc8b0eb28875aef559c1f316
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-12 12:33:22 +0200

    Eliza error page created

--- frontend/src/Pages/ElizaError/ElizaError.jsx
+++ frontend/src/Pages/ElizaError/ElizaError.jsx
@@ -1,15 +1,18 @@
-import "./ElizaError.css"
+import "./ElizaError.css";
 
 export default function ElizaError() {
-
   return (
-    <main className="error-eliza">
-      <div className="corporation">MK Ultra Corp.</div>
-      <section className="central-card">
-        <p>ERROR AL ABRIR LA CONSOLA</p>
-        <p>Eliza System Failure</p>
-        <p>Cargando...</p>
-      </section>
+    <main className="error-eliza-container">
+      <div className="error-eliza">
+        <div className="corporation">MKUltra Corp.</div>
+        <div className="card-container">
+          <section className="central-card">
+            <p>ERROR AL ABRIR LA CONSOLA</p>
+            <p>Eliza System Failure</p>
+            <p>Cargando...</p>
+          </section>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
