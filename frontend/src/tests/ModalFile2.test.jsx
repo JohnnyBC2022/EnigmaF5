01fa72bd704d8c41f17977e26aeddbf897e945a6
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-04-24 14:57:46 +0200

    Improved tests for Modal File 2

--- frontend/src/tests/ModalFile2.test.jsx
+++ frontend/src/tests/ModalFile2.test.jsx
@@ -1,22 +1,30 @@
 import { render, fireEvent } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
 import ModalFile2 from "../pages/ModalFile2/ModalFile2";
 
 test("If password is not correct you recieve an error message ", () => {
-  const { getByLabelText, getByText } = render(
-    <Router>
-      <ModalFile2 />
-    </Router>
-  );
+  const { getByLabelText, getByText } = render(<ModalFile2 />);
+
+  const input = getByLabelText("Password:");
+
+  fireEvent.change(input, { target: { value: "estupida" } });
+
+  fireEvent.click(getByText("[Enviar]", { selector: "button" }));
+
+  expect(getByText("* Contraseña incorrecta")).toBeInTheDocument();
+});
+
+test("If password is correct you get send to next page ", () => {
+  const { getByLabelText, getByText } = render(<ModalFile2 />);
 
   const input = getByLabelText("Password:");
 
   fireEvent.change(input, { target: { value: "weizenbaum" } });
 
-  fireEvent.click(getByText("[Enviar]", {selector: "button"}));
+  fireEvent.click(getByText("[Enviar]", { selector: "button" }));
 
-  expect(getByText("* Contraseña incorrecta")).toBeInTheDocument();
+  expect(
+    getByText(
+      /01110011 01101111 01101100 01100101 00100000 01100011 01101111 01100100 01100101 00101000 00110111 00111000 00111001 00101101 00110001 01011000 01000001 00101001/
+    )
+  ).toBeInTheDocument();
 });
-
-
-
