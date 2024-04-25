1ed0bbc3e487f0d0a89017135e3ea6297bb0c607
Johnny <jonathanbc2012@gmail.com>
2024-04-25 15:32:34 +0200

    initial conversation added to console.jsx

--- frontend/src/presentation/components/ModalFile1/ModalFile1.jsx
+++ frontend/src/presentation/components/ModalFile1/ModalFile1.jsx
@@ -2,11 +2,11 @@ import "./ModalFile1.css";
 import file1Img from "../../assets/images/file1Img.jpg";
 import { ImageMagnifier } from "../ImageMagnifier/ImageMagnifier";
 
-export const ModalFile1 = ({ closeModal }) => {
+export const ModalFile1 = ({ onClose }) => {
   return (
-    <div className="modalOverlay1" onClick={closeModal}>
+    <div className="modalOverlay1" onClick={onClose}>
       <div className="modalContent1" onClick={(e) => e.stopPropagation()}>
-        <button className="closeModalButton1" onClick={closeModal}>
+        <button className="closeModalButton1" onClick={onClose}>
           <p>X</p>
         </button>
         <div>
--- frontend/src/presentation/pages/Consola/Consola.jsx
+++ frontend/src/presentation/pages/Consola/Consola.jsx
@@ -1,29 +1,33 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./Consola.css";
 import { ModalFile1 } from "../../components/ModalFile1/ModalFile1";
-import  ModalFile2  from "../../components/ModalFile2/ModalFile2";
+import ModalFile2 from "../../components/ModalFile2/ModalFile2";
 
 export const Consola = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [messageHistory, setMessageHistory] = useState([]);
   const [showModalFile1, setShowModalFile1] = useState(false);
   const [showModalFile2, setShowModalFile2] = useState(false);
-  const [inputValue, setInputValue] = useState("");
   const [isInputFocused, setIsInputFocused] = useState(false);
-
-  const handleModalFile1Click = () => {
-    setShowModalFile1(true);
-  };
-  
-
-  const closeModalFile1 = () => {
-    setShowModalFile1(false);
-  };
-  const handleModalFile2Click = () => {
-    setShowModalFile2(true);
-    closeModalFile1();
-  };
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
+  };
+
+  const handleInputSubmit = (event) => {
+    event.preventDefault();
+
+    // Almacenar el mensaje del usuario en el historial
+    setMessageHistory((prevHistory) => [
+      ...prevHistory,
+      { type: "user", message: inputValue },
+    ]);
+
+    // Lógica para manejar el comando ingresado
+    handleCommand(inputValue);
+
+    // Limpiar el valor del input
+    setInputValue("");
   };
 
   const handleInputFocus = () => {
@@ -33,6 +37,56 @@ export const Consola = () => {
 
   const handleInputBlur = () => {
     setIsInputFocused(false);
+  };
+
+  const handleCommand = (command) => {
+    // Lógica para manejar los comandos ingresados
+    if (command === "eliza mad-slip:run") {
+      // Simulación de respuesta de Eliza
+      const elizaMessage = (
+        <span className="elizaText">
+          BIENVENIDO A<br />
+          <br />
+          <div>
+            EEEEEE
+            LL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;IIII&nbsp;ZZZZZZZ&nbsp;&nbsp;AAAAA
+            <br />
+            EE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;LL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;II&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ZZ&nbsp;&nbsp;AA&nbsp;&nbsp;&nbsp;AA
+            <br />
+            EEEEEE&nbsp;LL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;II&nbsp;&nbsp;&nbsp;&nbsp;ZZZ&nbsp;&nbsp;&nbsp;AAAAAAA
+            <br />
+            EE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;LL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;II&nbsp;&nbsp;&nbsp;ZZ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AA&nbsp;&nbsp;&nbsp;AA
+            <br />
+            EEEEEE LLLLLL IIII ZZZZZZZ AA&nbsp;&nbsp;&nbsp;AA
+            <br />
+          </div>
+          <br />
+          Copyright (C) 1964, 1967, MIT.
+          <br />
+          All rights reserved.
+          <br />
+          <br />
+          <span className="elizaText">ELIZA</span>: Hola, mi nombre es Eliza.
+          Soy un novedoso programa de inteligencia artificial. Puedo responder a
+          las entradas del usuario con preguntas y respuestas. ¿Cómo está usted
+          hoy?
+        </span>
+      );
+      // Almacenar el mensaje de Eliza en el historial
+      setMessageHistory((prevHistory) => [
+        ...prevHistory,
+        { type: "eliza", message: elizaMessage },
+      ]);
+    } else {
+      // Simulación de mensaje de error para comando incorrecto
+      const errorMessage =
+        "Comando incorrecto. Introduzca el comando correcto para ejecutar Eliza.";
+      // Almacenar el mensaje de Parry en el historial
+      setMessageHistory((prevHistory) => [
+        ...prevHistory,
+        { type: "parry", message: errorMessage },
+      ]);
+    }
   };
 
   return (
@@ -47,22 +101,40 @@ export const Consola = () => {
               <p>&nbsp;</p>
             </div>
             <div className="filesRight">
-              <button onClick={handleModalFile1Click}>File 1</button>
-              <button onClick={handleModalFile2Click}>File 2</button>
-              {showModalFile2 && <ModalFile2 onClose={() => setShowModalFile2(false)} />}
+              <button onClick={() => setShowModalFile1(true)}>File 1</button>
+              {showModalFile1 && (
+                <ModalFile1 onClose={() => setShowModalFile1(false)} />
+              )}
+              <button onClick={() => setShowModalFile2(true)}>File 2</button>
+              {showModalFile2 && (
+                <ModalFile2 onClose={() => setShowModalFile2(false)} />
+              )}
             </div>
           </div>
         </div>
         <div className="fileInfoRight">
           <p>
-            Welcome to QDOS version alpha.
-            <p>Copyright 1968.</p>
-            <p>Malibu Artifactual Intelligence, Inc.</p>
+            <span>Welcome to QDOS version alpha.</span>
+            <br />
+            <br />
+            Copyright 1968.
+            <br />
+            Malibu Artifactual Intelligence, Inc.
+            <br />
+            <br />
+            <span className="flicker1">Select a file to continue...</span>
           </p>
-          <p className="flicker1">Select a file to continue...</p>
         </div>
       </div>
-      <div className="consoleInputContainer">
+      <div className="consoleHistory">
+        {/* Historial de mensajes */}
+        {messageHistory.map((message, index) => (
+          <p key={index} className={message.type}>
+            {message.message}
+          </p>
+        ))}
+      </div>
+      <form onSubmit={handleInputSubmit} className="consoleInputContainer">
         <label className="consoleLabel">c:\&gt;</label>
         <input
           className={`consoleInput ${isInputFocused ? "" : "flicker2"}`}
@@ -73,8 +145,7 @@ export const Consola = () => {
           onFocus={handleInputFocus}
           onBlur={handleInputBlur}
         />
-      </div>
-      {showModalFile1 && <ModalFile1 closeModal={closeModalFile1} />}
+      </form>
     </div>
   );
 };
