b8578cd6afd152c47c297d017c22e3204b6fc2b3
San-QP <s.rebollar.g@gmail.com>
2024-04-25 15:20:06 +0200

    Add progress bar components

---
+++ frontend/src/presentation/components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,9 @@
+import "./ProgressBar.css";
+
+export const ProgressBar = () => {
+  return (
+    <div className="containerBar">
+      <div className="progressBar1"></div>
+    </div>
+  );
+};
---
+++ frontend/src/presentation/components/ProgressBar/ProgressBar3.jsx
@@ -0,0 +1,9 @@
+import "./ProgressBar.css";
+
+export const ProgressBar3 = () => {
+  return (
+    <div className="containerBar">
+      <div className="progressBar3"></div>
+    </div>
+  );
+};
