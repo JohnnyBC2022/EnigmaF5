0e19ab2aacea9dcb2430e921191ac7491e7a5e34
Johnny <jonathanbc2012@gmail.com>
2024-04-29 15:22:55 +0200

    deploy in vercel

--- backend/src/main/java/com/enigmaf5/backend/controllers/QuestionController.java
+++ backend/src/main/java/com/enigmaf5/backend/controllers/QuestionController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/questions")
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://enigmaf5.vercel.app/"})
 public class QuestionController {
 
     private final QuestionService questionService;
